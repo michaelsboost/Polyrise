e235ea91a20143c9609b09701caa82f05cda4272
Michael <mikethedj4@yahoo.com>
2018-07-03 08:04:48 -0500

    new blocks

--- js/polyrise.js
+++ js/polyrise.js
@@ -1,9 +1,8 @@
 // Version: 0.0.1
 // Polyrise, copyright (c) by Michael Schwartz and others
 // Distributed under an MIT license: https://github.com/michaelsboost/Polyrise/blob/gh-pages/LICENSE
-
+// 
 // This is Polyrise (https://michaelsboost.github.io/Polyrise/), Free Mobile Website Builder Software
-// implemented in JavaScript!
 
 // fadeIn background-overlay
 $(".bg-overlay, .bg-overlay2, .bg-overlay3").fadeIn();
