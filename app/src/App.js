d6a45f018cd1bafa254bb5c010d7933d099e6028
michaelsboost <schwartzm1992@gmail.com>
2025-04-18 23:07:29 -0500

    removed donate button

--- app/src/App.js
+++ app/src/App.js
@@ -1022,9 +1022,6 @@ const icons = (function() {
          id="path4"
          clip-path="url(#clipPath7)" /></g></g>
     </svg>`,
-    heart: `<svg class="w-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
-    </svg>`,
     plus: `<svg class="${SidebarIconCSS}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
     </svg>`,
@@ -1225,17 +1222,6 @@ function LeftMenubar() {
       href="https://twitter.com/michaelsboost"
     >
       ${icons.twitter}
-    </a>
-  </li>
-  <li class="list-none m-0">
-    <a
-      href="https://michaelsboost.com/donate/"
-      aria-label="Donation helps developer maintence"
-      target="_blank"
-      role="button"
-      class="${buttonSize} text-sm border-0 px-4 py-3 rounded-md bg-transparent text-red-400"
-    >
-      ${icons.heart}
     </a>
   </li>
   <li class="list-none m-0">
